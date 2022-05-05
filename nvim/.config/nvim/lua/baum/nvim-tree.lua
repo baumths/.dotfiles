a11a74badfb7e539d7db6c52f7f652b545105472
mbaumgartenbr <mbaumgartenbr@gmail.com>
2022-05-04 21:54:50 -0300

    Fix nvim plugin warnings

--- nvim/.config/nvim/lua/baum/nvim-tree.lua
+++ nvim/.config/nvim/lua/baum/nvim-tree.lua
@@ -54,14 +54,9 @@ nvim_tree.setup {
     "dashboard",
     "alpha",
   },
-  auto_close = false,
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = false,
-  update_to_buf_dir = {
-    enable = false,
-    auto_open = false,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -91,7 +86,6 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
@@ -103,16 +97,6 @@ nvim_tree.setup {
     },
     number = false,
     relativenumber = false,
-  },
-  git_hl = true,
-  disable_window_picker = false,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
   },
   actions = {
     change_dir = {

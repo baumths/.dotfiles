29e417860761313a14debb03d10a011ab58c6b74
mbaumgartenbr <mbaumgartenbr@gmail.com>
2022-05-12 19:49:18 -0300

    feat(scripts): extract ubuntu install script

---
+++ scripts/distros/ubuntu.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+add_gpg_key() {
+  local URL="$1"
+  local FILE_NAME="$2"
+  local FILE_PATH="/usr/share/keyrings/$FILE_NAME"
+
+  if [[ -f "$FILE_PATH" ]]; then
+    echo "[*] GPG key [$FILE_NAME] already exists, skipping..."
+  else
+    wget -O- $URL | sudo gpg --dearmor -o $FILE_PATH
+    echo "[*] GPG key [$FILE_NAME] added."
+  fi
+}
+
+add_apt_source() {
+  local FILE_NAME="$1"
+  local CONTENT="$2"
+  local FILE_PATH="/etc/apt/sources.list.d/$FILE_NAME"
+
+  if [[ -f "$FILE_PATH" ]]; then
+    echo "[*] Apt source [$FILE_NAME] already exists, skipping..."
+  else
+    echo $CONTENT | sudo tee $FILE_PATH > /dev/null
+    echo "[*] Apt source [$FILE_NAME] added."
+  fi
+}
+
+install_packages() {
+  sudo apt install -yy \
+    zsh \
+    fzf \
+    stow \
+    wget \
+    curl \
+    tree \
+    gnupg \
+    kitty \
+    lua5.4 \
+    software-properties-common \
+    apt-transport-https \
+    ca-certificates \
+    gnome-tweaks \
+    gnome-shell-extensions \
+    clang \
+    cmake \
+    ninja-build \
+    pkg-config \
+    libgtk-3-dev \
+    golang
+}
+
+install_neovim() {
+  sudo add-apt-repository ppa:neovim-ppa/unstable -yy
+  sudo apt update
+  sudo apt install -yy neovim
+}
+
+install_lazygit() {
+  go install github.com/jesseduffield/lazygit@latest
+}
+
+install_docker() {
+  local GPG_KEY_LINK="https://download.docker.com/linux/ubuntu/gpg" 
+  local GPG_KEY_FILE_NAME="docker-archive-keyring.gpg"
+  local APT_SOURCE_FILE_NAME="docker.list"
+
+  add_gpg_key "$GPG_KEY_LINK" "$GPG_KEY_FILE_NAME"
+
+  local CONTENT=""
+  CONTENT+="deb [arch=$(dpkg --print-architecture) "
+  CONTENT+="signed-by=/usr/share/keyrings/$GPG_KEY_FILE_NAME] "
+  CONTENT+="https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+  add_apt_source "$APT_SOURCE_FILE_NAME" "$CONTENT"
+
+  sudo apt update
+  sudo apt install -yy \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-compose-plugin
+
+  sudo groupadd docker
+  sudo usermod -aG docker "$USER"
+}
+
+install_flutter() {
+  git clone -b stable --depth 1 https://github.com/flutter/flutter.git "$HOME/.local/share/flutter"
+}
+
+install_chrome() {
+  local GPG_KEY_LINK="https://dl-ssl.google.com/linux/linux_signing_key.pub" 
+  local GPG_KEY_FILE_NAME="google-chrome.gpg"
+  local APT_SOURCE_FILE_NAME="google-chrome.list"
+
+  add_gpg_key "$GPG_KEY_LINK" "$GPG_KEY_FILE_NAME"
+
+  local CONTENT=""
+  CONTENT+="deb [arch=$(dpkg --print-architecture) "
+  CONTENT+="signed-by=/usr/share/keyrings/$GPG_KEY_FILE_NAME] "
+  CONTENT+="http://dl.google.com/linux/chrome/deb/ stable main"
+
+  add_apt_source "$APT_SOURCE_FILE_NAME" "$CONTENT"
+
+  sudo apt update
+  sudo apt install -yy google-chrome-stable
+}
+
+main () {
+  echo -e "\n[*] Installing apt Packages..."
+  install_packages
+  
+  echo -e "\n[*] Installing Neovim..."
+  install_neovim
+
+  echo -e "\n[*] Installing Lazygit..."
+  install_lazygit
+
+  echo -e "\n[*] Installing Docker..."
+  install_docker
+
+  echo -e "\n[*] Installing Chrome..."
+  install_chrome
+
+  echo -e "\n[*] Installing Flutter..."
+  install_flutter
+}
+
+main "$@"
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,120 +1,25 @@
 #!/bin/bash
 
-# HELPERS
+setup_symlinks() {
+  pushd "$HOME/.dotfiles" &> /dev/null
 
-add_gpg_key() {
-  local link="$1"
-  local file_name="$2"
-  local file_path="/usr/share/keyrings/$file_name"
+  if [ $? -eq 0 ]; then
+    echo "[*] Stowing .dotfiles..."
 
-  if [[ -f "$file_path" ]]; then
-    echo "[*] GPG key [$file_name] already exists, skipping..."
+    stow -v git/
+    stow -v zsh/
+    stow -v nvim/
+    stow -v kitty/
+    stow -v lazygit/
+
+    popd &> /dev/null
+    echo "[*] Done Stowing .dotfiles."
   else
-    wget -O- $link | sudo gpg --dearmor -o $file_path
-    echo "[*] GPG key [$file_name] added."
+    echo "[!] Could not find '.dotfiles' directory at '$HOME'"
   fi
 }
 
-add_apt_source() {
-  local file_name="$1"
-  local content="$2"
-  local file_path="/etc/apt/sources.list.d/$file_name"
-
-  if [[ -f "$file_path" ]]; then
-    echo "[*] Apt source [$file_name] already exists, skipping..."
-  else
-    echo $content | sudo tee $file_path > /dev/null
-    echo "[*] Apt source [$file_name] added."
-  fi
-}
-
-# INSTALLERS
-
-install_apt_pkgs() {
-  sudo apt install -yy \
-    zsh \
-    fzf \
-    stow \
-    wget \
-    curl \
-    tree \
-    gnupg \
-    kitty \
-    lua5.4 \
-    software-properties-common \
-    apt-transport-https \
-    ca-certificates \
-    lsb-release \
-    gnome-tweaks \
-    gnome-shell-extensions \
-    clang \
-    cmake \
-    ninja-build \
-    pkg-config \
-    libgtk-3-dev \
-    golang
-}
-
-install_neovim() {
-  sudo add-apt-repository ppa:neovim-ppa/unstable -yy
-  sudo apt update
-  sudo apt install -yy neovim
-}
-
-install_lazygit() {
-  go install github.com/jesseduffield/lazygit@latest
-}
-
-install_docker() {
-  local gpg_key_link="https://download.docker.com/linux/ubuntu/gpg" 
-  local gpg_key_file_name="docker-archive-keyring.gpg"
-  local apt_source_file_name="docker.list"
-
-  add_gpg_key "$gpg_key_link" "$gpg_key_file_name"
-
-  local content=""
-  content+="deb [arch=$(dpkg --print-architecture) "
-  content+="signed-by=/usr/share/keyrings/$gpg_key_file_name] "
-  content+="https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-  add_apt_source "$apt_source_file_name" "$content"
-
-  sudo apt update
-  sudo apt install -yy \
-    docker-ce \
-    docker-ce-cli \
-    containerd.io \
-    docker-compose-plugin
-
-  sudo groupadd docker
-  sudo usermod -aG docker "$USER"
-}
-
-install_flutter() {
-  git clone -b stable --depth 1 https://github.com/flutter/flutter.git "$HOME/.local/share/flutter"
-}
-
-install_chrome() {
-  local gpg_key_link="https://dl-ssl.google.com/linux/linux_signing_key.pub" 
-  local gpg_key_file_name="google-chrome.gpg"
-  local apt_source_file_name="google-chrome.list"
-
-  add_gpg_key "$gpg_key_link" "$gpg_key_file_name"
-
-  local content=""
-  content+="deb [arch=$(dpkg --print-architecture) "
-  content+="signed-by=/usr/share/keyrings/$gpg_key_file_name] "
-  content+="http://dl.google.com/linux/chrome/deb/ stable main"
-
-  add_apt_source "$apt_source_file_name" "$content"
-
-  sudo apt update
-  sudo apt install -yy google-chrome-stable
-}
-
-# SETUP
-
-setup_shell() {
+setup_gnome_settings() {
   # ALT + RETURN = TERMINAL
   gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Alt>Return']"
 
@@ -147,46 +52,42 @@ setup_shell() {
   fi
 }
 
-setup_symlinks() {
-  pushd "$HOME/.dotfiles"
+run_install_script() {
+  local FILE_NAME="$1"
+  local FILE_PATH="$HOME/.dotfiles/scripts/distros/$FILE_NAME"
 
-  stow -v git/
-  stow -v zsh/
-  stow -v nvim/
-  stow -v kitty/
-  stow -v lazygit/
-
-  popd
+  if [ -f "$FILE_PATH" ]; then
+    chmod +x $FILE_PATH
+    bash $FILE_PATH
+  else
+    echo -e "\n[!] Could not find script at $FILE_PATH\n"
+    exit 1
+  fi
 }
 
-# Entrypoint
-main () {
-  echo -e "\n[*] Installing apt packages..."
-  install_apt_pkgs
-  
-  echo -e "\n[*] Creating symlinks..."
+post_install() {
+  echo -e ""
   setup_symlinks
-  
-  echo -e "\n[*] Installing Neovim..."
-  install_neovim
 
-  echo -e "\n[*] Installing Lazygit..."
-  install_lazygit
+  echo -e "\n[*] Updating Gnome shell settings..."
+  setup_gnome_settings
 
-  echo -e "\n[*] Installing Docker..."
-  install_docker
-
-  echo -e "\n[*] Installing Chrome..."
-  install_chrome
-
-  echo -e "\n[*] Installing Flutter..."
-  install_flutter
-
-  echo -e "\n[*] Updating Gnome Themes..."
-  setup_shell
-
-  echo "[*] Set default shell to zsh"
+  echo "[*] Setting 'zsh' as the default shell..."
   chsh -s $(which zsh)
+}
+
+main() {
+  case "$(lsb_release -is)" in
+    Ubuntu)
+      run_install_script "ubuntu.sh"
+      ;;
+    *)
+      echo -e "\n[!] Unknown distro, skipping...\n"
+      exit 1
+      ;;
+  esac
+
+  post_install
 
   echo -e "\n[#] Remember to restart your computer once done."
 }

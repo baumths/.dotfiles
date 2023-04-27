a071a2ab4371076f0b475823a1aaaf8599e94cd7
baumths <baumths@gmail.com>
2023-04-27 20:22:54 -0300

    feat(zsh): add android tooling to PATH

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -8,8 +8,13 @@ export EDITOR="hx"
 export VISUAL="hx"
 export MANPAGER="nvim +Man!"
 
+export ANDROID_HOME="$HOME/.local/share/android"
+
 export PATH="$PATH:$HOME/.pub-cache/bin" # Dart active global packages
 export PATH="$PATH:$HOME/.local/share/flutter/bin"
 export PATH="$PATH:$HOME/.local/share/lua/bin"
 export PATH="$PATH:$HOME/go/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
+
+export PATH="$PATH:$ANDROID_HOME/platform-tools"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
